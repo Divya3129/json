8f4f265b509858d6bf518b9c6feec73e8592fd6e
jh96 <36338184+jordan-hoang@users.noreply.github.com>
2024-11-18 14:23:49 -0800

    Update tests/src/unit-deserialization.cpp
    Co-authored-by: Niels Lohmann <niels.lohmann@gmail.com>

--- tests/src/unit-deserialization.cpp
+++ tests/src/unit-deserialization.cpp
@@ -365,7 +365,7 @@ TEST_CASE("deserialization")
             const char* string = nullptr;
             json _;
             CHECK_THROWS_WITH_AS(_ = json::parse(string), "[json.exception.parse_error.101] parse error: attempting to parse an empty input; check that your input string or stream contains the expected JSON", json::parse_error&);
-            CHECK_THROWS_WITH_AS(nlohmann::detail::input_adapter(nullptr), "[json.exception.parse_error.101] parse error: attempting to parse an empty input; check that your input string or stream contains the expected JSON", json::parse_error&);
+            CHECK_THROWS_WITH_AS(_ = json::parse(nullptr), "[json.exception.parse_error.101] parse error: attempting to parse an empty input; check that your input string or stream contains the expected JSON", json::parse_error&);
         }
 
         SECTION("operator<<")

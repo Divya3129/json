1dfb9e759a6ba67b9f37d21a8e42cd3e00586c10
Jordan <36338184+jordan-hoang@users.noreply.github.com>
2024-11-18 23:13:54 -0800

    rm duplicate includes

--- include/nlohmann/detail/input/input_adapters.hpp
+++ include/nlohmann/detail/input/input_adapters.hpp
@@ -18,7 +18,6 @@
 #include <type_traits> // enable_if, is_base_of, is_pointer, is_integral, remove_pointer
 #include <utility> // pair, declval
 #include <nlohmann/detail/exceptions.hpp>
-#include <nlohmann/detail/macro_scope.hpp>
 
 #ifndef JSON_NO_IO
     #include <cstdio>   // FILE *

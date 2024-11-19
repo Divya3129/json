7e302f7241dece442cfb0a94e864e3cc8136a562
Jordan <36338184+jordan-hoang@users.noreply.github.com>
2024-11-18 23:50:42 -0800

    moved it above

--- include/nlohmann/detail/input/input_adapters.hpp
+++ include/nlohmann/detail/input/input_adapters.hpp
@@ -23,8 +23,8 @@
     #include <istream>  // istream
 #endif                  // JSON_NO_IO
 
-#include <nlohmann/detail/iterators/iterator_traits.hpp>
 #include <nlohmann/detail/exceptions.hpp>
+#include <nlohmann/detail/iterators/iterator_traits.hpp>
 #include <nlohmann/detail/macro_scope.hpp>
 #include <nlohmann/detail/meta/type_traits.hpp>
 
